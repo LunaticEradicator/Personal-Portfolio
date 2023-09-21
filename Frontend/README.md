aaf9528b9955898c3ab3d2895801fcc1b656335e
YeduKrishnan A <kuttuzz17@gmail.com>
2023-09-21 12:56:03 +0530

    Initial commit

---
+++ Frontend/README.md
@@ -0,0 +1,1 @@
+Personal Portfolio WebSite Using MERN Stack.
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,6 @@
+// import React from "react";
+import "./sass/style.scss";
+
+export default function App() {
+  return <h1>Portfolio</h1>;
+}
